bd1e2ff66fadebadc7c1df1530177b6f64f396b8
David Charte <fdavidcl@protonmail.com>
2016-10-17 00:59:16 +0200

    Document rake helpers

--- rake_helpers.rb
+++ rake_helpers.rb
@@ -1,16 +1,28 @@
 # coding: utf-8
 
-# Helper functions
+#------------ Helper functions ------------#
+
+# pdf_for: Finds the paths of the target pdf files for a source code file
+#   Arguments:
+#     files: one or several filenames, or an array of filenames
+#   Outputs: an array of paths to the targeted pdf files
 def pdf_for *files
-  files.map do |f|
+  files.flatten.map do |f|
     ".out/" + f.sub(/.([^.]*?)$/, ".pdf")
   end
 end
 
+# sources_for: finds the path of the source file for a given pdf file and filetype
+#   Arguments:
+#     f: path to the pdf file
+#     type: extension of the source file
+#   Outputs: the path of the source file
 def sources_for f, type
   f.sub(".out/", "").sub(".pdf", ".#{type}")
 end
 
+# pandoc_rule: generates rules where pandoc handles the conversion to pdf
+#   Arguments: one or several file extensions
 def pandoc_rule *inputs
   # Generate PDFs from several sources with pandoc
   inputs.each do |i|
